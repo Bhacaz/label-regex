5d4bd763a31816763e848fb16a74e84d81b56ea2
Jean-Francis Bastien <bhacaz@gmail.com>
2021-02-12 20:14:42 -0500

    fix input

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const octokit = new Octokit({ auth: core.getInput('token') });
 
 const fieldToMatch = core.getInput('field');
 const regexToApply = new RegExp(core.getInput('regex'));
-const forceLowerCase = core.getInput('forcelowercase');
+const forceLowerCase = core.getInput('lowercase');
 const repository = process.env['GITHUB_REPOSITORY']
 const pullRequestNumber = parseInt(/refs\/pull\/(\d+?)\//.exec(process.env['GITHUB_REF'])[1])
 const owner = repository.split('/')[0]
