052d90e6ea39d51499359d33efbb3d2c898f8d7e
Jean-Francis Bastien <bhacaz@gmail.com>
2021-02-12 20:00:08 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({ auth: core.getInput('token') });
+
+const fieldToMatch = core.getInput('field');
+const regexToApply = new RegExp(core.getInput('regex'));
+const forceLowerCase = core.getInput('forcelowercase');
+const repository = process.env['GITHUB_REPOSITORY']
+const pullRequestNumber = parseInt(/refs\/pull\/(\d+?)\//.exec(process.env['GITHUB_REF'])[1])
+const owner = repository.split('/')[0]
+const repo = repository.split('/')[1]
+
+octokit.pulls.get({
+    owner,
+    repo,
+    pull_number: pullRequestNumber,
+  })
+  .then((data) => {
+      let labelToAssign = regexToApply.exec(data.data[fieldToMatch])[1]
+      if(labelToAssign) {
+        if(forceLowerCase) { labelToAssign = labelToAssign.toLowerCase() }
+         octokit.issues.addLabels({
+            owner,
+            repo,
+            issue_number: pullRequestNumber,
+            labels: [labelToAssign]
+        })
+      }
+  })
